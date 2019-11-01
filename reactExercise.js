c45e93479c7ba6e99c44462f880d259944e9d115
Victor Huynh <victor.h.huynh@gmail.com>
2019-11-01 18:58:20 -0400

    React: Override Default Props

--- reactExercise.js
+++ reactExercise.js
@@ -291,3 +291,36 @@
 //////////////////////////////////////////////////////////////////////////////////////
 // November 1st 2019 React: Use Default Propss //
 /////////////////////////////////////////////////////////////////////////////////////
+
+// const ShoppingCart = (props) => {
+//   return (
+//     <div>
+//       <h1>Shopping Cart Component</h1>
+//     </div>
+//   )
+// };
+// // change code below this line
+// ShoppingCart.defaultProps = { items: 0} 
+
+//////////////////////////////////////////////////////////////////////////////////////
+// November 1st 2019 React: Override Default Props //
+/////////////////////////////////////////////////////////////////////////////////////
+
+// const Items = (props) => {
+//   return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+// }
+
+// Items.defaultProps = {
+//   quantity: 0
+// }
+
+// class ShoppingCart extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     { /* change code below this line */ }
+//     return <Items quantity={10}/>
+//     { /* change code above this line */ }
+//   }
+// };
