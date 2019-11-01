51e3c3d986696ef0d4ccadd1a4c28916376ef277
Victor Huynh <victor.h.huynh@gmail.com>
2019-11-01 19:47:17 -0400

    React: Review Using Props with Stateless Functional Components

--- reactExercise.js
+++ reactExercise.js
@@ -388,3 +388,31 @@
 //     );
 //   }
 // };
+
+//////////////////////////////////////////////////////////////////////////////////////
+// November 1st 2019 React: Review Using Props with Stateless Functional Components //
+/////////////////////////////////////////////////////////////////////////////////////
+
+// class CampSite extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <Camper/>
+//       </div>
+//     );
+//   }
+// };
+// // change code below this line
+
+// const Camper = props => <p>{props.name}</p>
+
+// Camper.defaultProps = {
+//   name: "CamperBot"
+// };
+
+// Camper.propTypes = {
+//   name: PropTypes.string.isRequired
+// }
