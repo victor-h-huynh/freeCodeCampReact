60d3ba8a9256d6f970c03060a7da37596f84e7d6
Victor Huynh <victor.h.huynh@gmail.com>
2019-11-01 19:05:42 -0400

    React: Use PropTypes to Define the Props You Expect

--- reactExercise.js
+++ reactExercise.js
@@ -324,3 +324,28 @@
 //     { /* change code above this line */ }
 //   }
 // };
+
+//////////////////////////////////////////////////////////////////////////////////////
+// November 1st 2019 React: Use PropTypes to Define the Props You Expect //
+/////////////////////////////////////////////////////////////////////////////////////
+
+// const Items = (props) => {
+//   return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+// };
+
+// // change code below this line
+// Items.propTypes = { quantity: PropTypes.number.isRequired }
+// // change code above this line
+
+// Items.defaultProps = {
+//   quantity: 0
+// };
+
+// class ShoppingCart extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return <Items />
+//   }
+// };
