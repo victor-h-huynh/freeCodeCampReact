f686a67b928b48f7d7d33fad693e923021fa16bb
Victor Huynh <victor.h.huynh@gmail.com>
2019-11-01 17:34:06 -0400

    React: Render a Class Component to the DOM

--- reactExercise.js
+++ reactExercise.js
@@ -216,3 +216,27 @@
 //     );
 //   }
 // };
+
+//////////////////////////////////////////////////////////////////////////////////////
+// November 1st 2019 React: Render a Class Component to the DOM //
+/////////////////////////////////////////////////////////////////////////////////////
+
+// class TypesOfFood extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>Types of Food:</h1>
+//         {/* change code below this line */}
+//         <Fruits/>
+//         <Vegetables/>
+//         {/* change code above this line */}
+//       </div>
+//     );
+//   }
+// };
+
+// // change code below this line
+// ReactDOM.render(<TypesOfFood/>, document.getElementById("challenge-node"))
