aa195be44efb7ad45d1a6775473455aa0bb9fde4
Victor Huynh <victor.h.huynh@gmail.com>
2019-11-01 16:49:55 -0400

    React: Create a React Component

--- reactExercise.js
+++ reactExercise.js
@@ -84,3 +84,24 @@
 
 //   // change code above this line
 // }
+
+//////////////////////////////////////////////////////////////////////////////////////
+// November 1st 2019 React: Create a React Component //
+/////////////////////////////////////////////////////////////////////////////////////
+
+// class MyComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     // change code below this line
+// return (
+//   <div>
+//     <h1>Hello React!</h1>
+//   </div>
+// )
+
+
+//     // change code above this line
+//   }
+// };
