ba792e4850b2d3c3224a4f2923e8f32400e15562
Victor Huynh <victor.h.huynh@gmail.com>
2019-11-01 17:06:13 -0400

    React: Create a Component with Composition

--- reactExercise.js
+++ reactExercise.js
@@ -105,3 +105,31 @@
 //     // change code above this line
 //   }
 // };
+
+//////////////////////////////////////////////////////////////////////////////////////
+// November 1st 2019 React: Create a Component with Composition //
+/////////////////////////////////////////////////////////////////////////////////////
+
+const ChildComponent = () => {
+  return (
+    <div>
+      <p>I am the child</p>
+    </div>
+  );
+};
+
+class ParentComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>I am the parent</h1>
+        { /* change code below this line */ }
+        <ChildComponent/>
+        { /* change code above this line */ }
+      </div>
+    );
+  }
+};
